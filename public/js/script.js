d847bee0cf9d4972a8461b957b8879467a36c467
Retiishia <azizfarhan72@gmail.com>
2025-08-14 04:26:25 +0700

    belajar1

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+alert("Welcome to the JavaScript file!");
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Document is ready!");
+});
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Farhan Aziz']);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/artikel', function () {
+    return view('artikel');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
